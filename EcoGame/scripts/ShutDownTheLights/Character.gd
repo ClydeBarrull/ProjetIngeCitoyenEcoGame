e10b6a0b850e339567b8dbf2bc8cd7e768540fd2
Paul Bréon <paul.breon@viacesi.fr>
2024-06-25 15:10:58 +0200

    Collision update

--- EcoGame/scripts/ShutDownTheLights/Character.gd
+++ EcoGame/scripts/ShutDownTheLights/Character.gd
@@ -12,7 +12,7 @@ var time_since_last_direction_change = 0.0
 var next_direction_change_time = 0.0
 
 var anim_player
-var elevator_collision
+var collision_shape
 
 func _ready():
 	# Rendre le Sprite2D enfant invisible par défaut
@@ -28,16 +28,12 @@ func _ready():
 	set_next_direction_change()
 	
 	anim_player = $AnimationPlayer
-	
-	# Trouver le CollisionShape2D de l'ascenseur
-	elevator_collision = get_node("/root/LightsMiniGame/Building/Area2D/Elevator")
+	collision_shape = $StaticBody2D/CollisionShape2D
 
-	# Vérifier si le CollisionShape2D a été trouvé
-	if not elevator_collision:
-		printerr("Elevator CollisionShape2D not found!")
-	else:
-		# Connecter le signal de collision avec l'ascenseur
-		elevator_collision.connect("body_entered", Callable(self, "_on_Elevator_CollisionShape2D_body_entered"))
+	# Connecter le signal de collision du StaticBody2D
+	var area = collision_shape.get_parent().get_parent()  # Obtenir l'Area2D parent du CollisionShape2D
+	if area:
+		area.connect("body_entered", Callable(self, "_on_body_entered"))
 
 func _process(delta):
 	anim_player.play("CharacterMovement")
@@ -74,14 +70,7 @@ func show_character_sprite(visible):
 	print("Showing character sprite:", visible)
 	get_node("Sprite2D").visible = visible
 
-func _on_Elevator_CollisionShape2D_body_entered(body):
-	# Vérifier si la collision est avec l'ascenseur
-	print("Character entered the elevator")
-	# Masquer le Sprite2D
+func _on_body_entered(body):
+	# Masquer le Sprite2D lorsque le personnage entre en contact avec un autre collider
+	print("Character entered a collider")
 	show_character_sprite(false)
-		
-	# Faire attendre pendant un certain temps avant de montrer à nouveau le Sprite du personnage
-	await get_tree().create_timer(0.5).completed
-		
-	# Temps écoulé, montrer à nouveau le Sprite du personnage
-	show_character_sprite(true)
