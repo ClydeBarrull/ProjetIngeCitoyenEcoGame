9d2db395e2a33cbed37ae8676cf8442f2cb2cbc6
Paul Bréon <paul.breon@viacesi.fr>
2024-06-25 19:02:44 +0200

    Change stage

--- EcoGame/scripts/ShutDownTheLights/Character.gd
+++ EcoGame/scripts/ShutDownTheLights/Character.gd
@@ -4,15 +4,17 @@ class_name Character
 
 var speed = 100
 var direction = Vector2.RIGHT
-var left_limit = 200
-var right_limit = 500
+var left_limit = -200
+var right_limit = 200
 var change_direction_interval_min = 1.0  # Intervalle minimal de temps en secondes pour le changement de direction
 var change_direction_interval_max = 3.0  # Intervalle maximal de temps en secondes pour le changement de direction
 var time_since_last_direction_change = 0.0
 var next_direction_change_time = 0.0
 
 var anim_player
-var collision_shape
+
+# Liste des positions Y des différents étages
+var floors = [0, -160, -310, -440, -600]
 
 func _ready():
 	# Rendre le Sprite2D enfant invisible par défaut
@@ -28,12 +30,8 @@ func _ready():
 	set_next_direction_change()
 	
 	anim_player = $AnimationPlayer
-	collision_shape = $StaticBody2D/CollisionShape2D
-
-	# Connecter le signal de collision du StaticBody2D
-	var area = collision_shape.get_parent().get_parent()  # Obtenir l'Area2D parent du CollisionShape2D
-	if area:
-		area.connect("body_entered", Callable(self, "_on_body_entered"))
+	
+	move_to_random_floor()
 
 func _process(delta):
 	anim_player.play("CharacterMovement")
@@ -56,6 +54,10 @@ func _process(delta):
 		change_direction()
 		set_next_direction_change()
 	position = new_position
+	
+	# Vérifier si la position x est proche de 0 (plutôt que d'exactement 0)
+	if abs(position.x) < 1.0:  # Tolérance pour la vérification de proximité
+		move_to_random_floor()
 
 func change_direction():
 	# Changer de direction aléatoirement
@@ -70,7 +72,8 @@ func show_character_sprite(visible):
 	print("Showing character sprite:", visible)
 	get_node("Sprite2D").visible = visible
 
-func _on_body_entered(body):
-	# Masquer le Sprite2D lorsque le personnage entre en contact avec un autre collider
-	print("Character entered a collider")
-	show_character_sprite(false)
+func move_to_random_floor():
+	# Sélectionner un étage aléatoire et mettre à jour la position Y du personnage
+	var random_floor = floors[randi() % floors.size()]
+	position.y = random_floor
+	print("Moved to floor:", random_floor)
